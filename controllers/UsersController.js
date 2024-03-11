ea78d2eb31007eff0d8f5ea64927771985cc3982
Mwembe-Tsuma <mwembeemmanuel@gmail.com>
2024-03-11 15:49:33 +0000

    UsersController.js

---
+++ controllers/UsersController.js
@@ -0,0 +1,36 @@
+// controllers/UsersController.js
+import dbClient from '../utils/db';
+import sha1 from 'sha1';
+
+class UsersController {
+  static async postNew(req, res) {
+    const { email, password } = req.body;
+
+    if (!email) {
+      return res.status(400).json({ error: 'Missing email' });
+    }
+
+    if (!password) {
+      return res.status(400).json({ error: 'Missing password' });
+    }
+
+    const existingUser = await dbClient.db.collection('users').findOne({ email });
+    if (existingUser) {
+      return res.status(400).json({ error: 'Already exist' });
+    }
+
+    const hashedPassword = sha1(password);
+
+    const newUser = {
+      email,
+      password: hashedPassword,
+    };
+
+    const result = await dbClient.db.collection('users').insertOne(newUser);
+
+    const { _id, ...user } = result.ops[0];
+    return res.status(201).json({ id: _id, email: user.email });
+  }
+}
+
+export default UsersController;
